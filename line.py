3c4fe39f8bdbe38f700eb2374385abf22a1287b8
CHIA-JUI YEH <noname19930930@gmail.com>
2016-12-15 13:50:53 +0800

    modulize

--- line.py
+++ line.py
@@ -1,24 +1,38 @@
+import sys
 import Queue
 import threading
 import urllib
 import time
 
-URL = "https://tv-line.pstatic.net/global/read/"
-EPISODE = "TVCAST_2016_12_12_3/b679GyoiyRA9YZz_4Z-wZ-A4cA_rmcvideo_1080P_1920_5120_192-"
-KEY = "?__gda__=1481700700_e11b42ae335f01e647ac6968debe1d05"
+if len(sys.argv) < 4:
+	print 'Usage: <url> <path> <part>'
+	exit()
+
+URL = sys.argv[1]
+PATH = sys.argv[2]
+PART = sys.argv[3]
+
+# generate urls
+query_index = URL.index('?')
+ext_index = URL.index('.ts')
+file_len = URL[:ext_index][::-1].index('-')
+
+a = URL[:ext_index - file_len]
+b = URL[query_index:]
 
 class Line:
 	def __init__(self, index):
-		self.index = index
+		self.index = index.rjust(file_len, '0')
+
 	def run(self):
 		file = urllib.URLopener()
-		file.retrieve(URL + EPISODE + self.index + '.ts' + KEY, self.index + '.ts')
+		file.retrieve(a + self.index + '.ts' + b, PATH + PATH + self.index + '.ts')
 		print '{index}'.format(index = self.index)
 
 # init
 q = Queue.Queue()
 for i in range(0, 300):
-	q.put(Line('%06d' % i))
+	q.put(Line('%d' % i))
 
 def next():
 	while q.qsize() > 0:  
@@ -37,4 +51,4 @@ t3.start()
 t4.start()
 
 while t1.is_alive() or t2.is_alive() or t3.is_alive() or t4.is_alive():
-     time.sleep(1) 
+     time.sleep(1)
